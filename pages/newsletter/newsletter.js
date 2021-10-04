a0c7d62676a41678b8ac21ded93dbf914dbe6ee9
pmaliwat <pmaliwat@salesforce.com>
2021-10-04 08:44:02 -0700

    Move pages around

--- pages/newsletter/newsletter.js
+++ pages/newsletter/newsletter.js
@@ -1,15 +1,18 @@
-let tilData = [
+let data = {
+    companyName: 'Patrick Web Design Inc.',
+    til: [
     {
         "date": "2021-08-31",
         "title": "My Title",
         "info": "My Info"
     }
-];
+]
+};
 
 let app;
 
 window.onload = function() {
-    app = new Vue({ el: '#newsletter-container', data: { companyName: 'Patrick Web Design Inc.' } });
+    app = new Vue({ el: '#newsletter-container', data: data });
 
     console.log(TIL_DATA);
 };
